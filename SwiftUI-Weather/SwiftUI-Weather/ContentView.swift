361aaa84e18e9447373651727a2c10b70ed98966
Paul Evans <evansp2@gmail.com>
2023-09-13 00:27:04 -0400

    updates

---
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Paul Evans on 9/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
